ee8e108e8a5653e4911448f934f6b24a09c43d56
Lucien Shui <lucien@lucien.ink>
2019-09-22 23:11:41 +0800

    Project init

---
+++ src/main/java/ink/lucien/iamlucien/IAmLucienApplication.java
@@ -0,0 +1,13 @@
+package ink.lucien.iamlucien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAmLucienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IAmLucienApplication.class, args);
+    }
+
+}
